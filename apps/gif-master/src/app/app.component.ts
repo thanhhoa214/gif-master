6b05b7d6644fa39f01d7859683de03f0e3a86bbd
thanhhoa214 <hoa.nguyen@nng.bz>
2021-08-29 11:53:17 +0700

    initial commit

---
+++ apps/gif-master/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gif-master-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gif-master';
+}
