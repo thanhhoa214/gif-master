d1ebaa6508d7f32dee78351a5f0ec784bffb16fa
thanhhoa214 <hoa.nguyen@nng.bz>
2021-09-04 10:48:44 +0700

    feature: View PDF

--- libs/gif-master/ui/src/lib/navbar/navbar.module.ts
+++ libs/gif-master/ui/src/lib/navbar/navbar.module.ts
@@ -4,12 +4,13 @@ import { NavbarComponent } from './navbar.component';
 import { LanguageModule } from '@shared/language';
 import { TuiButtonModule, TuiHintModule, TuiSvgModule } from '@taiga-ui/core';
 import { RouterModule } from '@angular/router';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 const tuiModules = [TuiButtonModule, TuiSvgModule, TuiHintModule];
 
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule, LanguageModule, RouterModule, tuiModules],
+  imports: [CommonModule, LanguageModule, RouterModule, NgxExtendedPdfViewerModule, tuiModules],
   exports: [NavbarComponent]
 })
 export class NavbarModule {}
